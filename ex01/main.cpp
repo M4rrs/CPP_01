efcf4a5b3b9242504a300213b01e2d5215de93e5
Mars <almasnorazman2803@outlook.com>
2023-03-08 19:24:31 +0800

    horde

---
+++ ex01/main.cpp
@@ -0,0 +1,17 @@
+// #include "Zombie.hpp"
+#include <iostream>
+
+int main( void )
+{
+	std::string name;
+	int	N;
+
+	std::cout << "Zombie Horde [name]: " << std::flush;
+	std::cin >> name;
+	std::cout << "Horde Size: " << std::flush;
+	while (std::cin >> N)
+	{
+
+	}
+	std::cout << N << std::endl;
+}
