7472834fcd652812a0bf109e733c6c0cce67ca3f
Mars <almasnorazman2803@outlook.com>
2023-03-11 18:29:41 +0800

    sed

---
+++ ex04/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int replace( std::string buff, char **av )
+{
+	std::ofstream	newFile;
+	int	size;
+	int pos;
+	
+	size = buff.length();
+	newFile.open((std::string)av[1] + ".replace");
+	if (newFile.fail())
+	{
+		std::cout << "Failed to open a new file" << std::endl;
+		return (1);
+	}
+	for (int i = 0; i < size; i++)
+	{
+		pos = buff.find(av[2], )
+	}
+
+}
+
+int main( int ac, char **av )
+{
+	std::ifstream file;
+	std::string buff;
+	char c;
+
+	if (ac != 4)
+	{
+		std::cout << "Incorrect number of arguments:\n" 
+				<< "[ ./sed <filename> <string to replace> <replacement string> ]" << std::endl;
+		return (0);
+	}
+	file.open(av[1]);
+	if (file.fail())
+	{
+		std::cout << "Error opening file." << std::endl;
+		return (0);
+	}
+	while (!file.eof() && file >> std::noskipws >> c)
+		buff += c;
+	std::cout << buff << std::flush;
+	file.close();
+	return (replace(buff, av));
+}
