517fb7ed22a41c3583db8d3c5a1be344743b8321
Mars <almasnorazman2803@outlook.com>
2023-03-14 18:52:43 +0800

    done

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -8,32 +8,37 @@ Harl::~Harl(){}
 
 void	Harl::debug( void )
 {
-	std::cout << "[DEBUG]\t\"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!\""
+	std::cout << "[DEBUG]\n"
+			<< "\"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!\"\n"
 			<< std::endl;
 }
 
 void	Harl::info( void )
 {
-	std::cout << "[INFO]\t\"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\""
+	std::cout << "[INFO]\n"
+			<< "\"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\"\n"
 			<< std::endl;
 }
 
 void	Harl::warning( void )
 {
-	std::cout << "[WARNING]\t\"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\""
+	std::cout << "[WARNING]\n"
+			<< "\"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\"\n"
 			<< std::endl;
 }
 
 void	Harl::error( void )
 {
-	std::cout << "[ERROR]\t\"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
+	std::cout << "[ERROR]\n"
+			<< "\"This is unacceptable! I want to speak to the manager now.\""
+			<< std::endl;
 }
 
 /*============	 PUBLIC	============*/
 
 void	Harl::complain( std::string level )
 {
-	int index = 0;
+	int index = -1;
 	std::string arr[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
 	for (int i = 0; i < 4; i++)
